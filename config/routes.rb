d47d6816f1a99337e0074c6e956a6a68610c8478
cris-gar <cristian.garrido@ceinf.cl>
2024-07-26 22:39:53 -0400

    add user controllers and init sessions

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "jokes#index"
+
+
+
+  get 'home/index'
+
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  resource :users
+  get '/users/:id', :to => 'users#edit', :as => :user
+  patch '/users/:id', :to => 'users#update'
+
+  namespace :authentication, path: '', as: '' do
+    resources :sessions, only: [:destroy]
+    get 'auth' => 'sessions#auth'
+    get 'login' => 'sessions#login'
+  end
 end
